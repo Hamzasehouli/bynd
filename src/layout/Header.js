f868e0fa8715eeedc5958df6d643dbf4f4180b4b
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-23 16:48:02 +0100

    Starting nav style

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,5 +1,38 @@
+import classes from "./Header.module.css";
+
 const Header = function () {
-  return <p>header</p>;
+  return (
+    <header>
+      <nav className={classes.nav}>
+        <ul className={classes.nav__links}>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <a href="">
+          {/* <img src="#"></img> */}
+          <p>bynd</p>
+        </a>
+        <ul className={classes.nav__links}>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

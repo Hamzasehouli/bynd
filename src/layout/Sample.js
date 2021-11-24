c759636d2c013794a24a084e7b271656019d5bf6
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-24 13:34:15 +0100

    Finished sample section

--- src/layout/Sample.js
+++ src/layout/Sample.js
@@ -1,14 +1,49 @@
 import classes from "./Sample.module.css";
+import Button from "../components/Button";
+import image from "../images/black.jpg";
+import { useState } from "react";
 const Sample = function () {
+  const [products, setProduct] = useState([
+    {
+      img: "",
+      title: "Sneaker",
+      price: "110.99",
+    },
+    {
+      img: "",
+      title: "Wanderes",
+      price: "100.99",
+    },
+    {
+      img: "",
+      title: "Icehikers",
+      price: "99.99",
+    },
+  ]);
   return (
-    <>
-      <h2>
-        We make your shoes more sustainable and{" "}
-        <span className={classes.wave}>environement friendly</span> and made
-        with clean
-        <span className={classes.wave}>ingredients.</span>
-      </h2>
-    </>
+    <section className={`${classes.sample}`}>
+      <div className="container section">
+        <h2 style={{ lineHeight: 170 + "%" }}>
+          We make your shoes more sustainable and{" "}
+          <span className={classes.wave}>environement friendly</span> and made
+          with clean <span className={classes.wave}>ingredients.</span>
+        </h2>
+      </div>
+      <ul className={`${classes.productList}`}>
+        {products.map((p) => (
+          <li className={classes.productItem}>
+            <figure className={classes.productFigure}>
+              <img alt="ss" src={image} className={classes.productImage}></img>
+            </figure>
+            <h3>{p.title}</h3>
+            <p className={classes.productPrice}>USD {p.price}</p>
+            <Button styl="outline" type="button">
+              Shop Now
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 

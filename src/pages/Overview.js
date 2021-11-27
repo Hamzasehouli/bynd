dadf697ffc929e7812022d04ad245bc8ea4233d7
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-11-27 20:23:19 +0100

    Finished parallax.js effect in the Learn section

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,12 +1,7 @@
-import { useContext } from "react";
 import Sample from "../layout/Sample";
 import Learn from "../layout/Learn";
-import { Appcontext } from "../store/Context";
 
 const Overview = function () {
-  const ctx = useContext(Appcontext);
-
-  console.log(ctx.name);
   return (
     <>
       <Sample />

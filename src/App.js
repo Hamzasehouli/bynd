069c0c47ccd6f2db4871633a6fa80b53d2bb2b4d
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-29 10:10:45 +0100

    to be merged

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Overview from "./pages/Overview";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
@@ -24,6 +25,9 @@ function App() {
             </Route> */}
             <Route path="/profile">
               <p>profile</p>
+            </Route>
+            <Route path="/account/:login">
+              <Login></Login>
             </Route>
             <Route path="*">
               <p>errro</p>
